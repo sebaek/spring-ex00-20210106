790d1f2c67a26e6c89e3c0f3509f431665c3c860
sebaek <sebaek@gmail.com>
2021-01-07 12:25:49 +0900

    mybatis

---
+++ src/main/java/org/zerock/mapper/TimeMapper.java
@@ -0,0 +1,19 @@
+package org.zerock.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TimeMapper {
+	
+	@Select("SELECT sysdate FROM dual")
+	public String getTime();
+}
+
+// 클래스 로딩
+// 커넥션 열고
+// statment 얻고
+// query 실행
+
+// SELECT sysdate FROM dual
+
+// resultSet 처리
+// 커넥션 닫고
---
+++ src/test/java/org/zerock/persistence/TimeMapperTests.java
@@ -0,0 +1,14 @@
+package org.zerock.persistence;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimeMapperTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
